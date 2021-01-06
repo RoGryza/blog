82d4744378c6fe4b69dcecb388b64b769ea18898
Rodrigo Gryzinski <rogryza@gmail.com>
2021-01-06 18:48:41 -0300

    nix flakes + zola init

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "rogryza.me blog";
+
+  inputs.nixpkgs.url = github:NixOs/nixpkgs/nixos-unstable;
+
+  outputs = { self, nixpkgs }:
+  let
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = with pkgs; [ zola ];
+    };
+  };
+}
