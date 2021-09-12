0abf7a7152b7981a9104e835310827256116501c
Rodrigo Gryzinski <contact@rogryza.me>
2021-09-12 19:38:34 +0100

    Update cloudflare worker

--- workers-site/index.js
+++ workers-site/index.js
@@ -1,4 +1,5 @@
-import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+import { getAssetFromKV, NotFoundError } from '@cloudflare/kv-asset-handler';
+import acceptLanguageParser from 'accept-language-parser';
 
 /**
  * The DEBUG flag will do two things that help during development:
@@ -9,10 +10,14 @@ import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
  */
 const DEBUG = false
 
+const SUPPORTED_LANGUAGES = ['en', 'pt'];
+
 addEventListener('fetch', event => {
   try {
     event.respondWith(handleEvent(event))
   } catch (e) {
+    console.error("Failed to respond", e);
+
     if (DEBUG) {
       return event.respondWith(
         new Response(e.message || e.toString(), {
@@ -20,21 +25,15 @@ addEventListener('fetch', event => {
         }),
       )
     }
+
     event.respondWith(new Response('Internal Error', { status: 500 }))
   }
 })
 
 async function handleEvent(event) {
-  const url = new URL(event.request.url)
-  let options = {}
-
-  /**
-   * You can add custom logic to how we fetch your assets
-   * by configuring the function `mapRequestToAsset`
-   */
-  // options.mapRequestToAsset = handlePrefix(/^\/docs/)
-
   try {
+    const options = {};
+
     if (DEBUG) {
       // customize caching
       options.cacheControl = {
@@ -42,52 +41,77 @@ async function handleEvent(event) {
       }
     }
 
-    const page = await getAssetFromKV(event, options)
-
-    // allow headers to be altered
-    const response = new Response(page.body, page)
-
-    response.headers.set('X-XSS-Protection', '1; mode=block')
-    response.headers.set('X-Content-Type-Options', 'nosniff')
-    response.headers.set('X-Frame-Options', 'DENY')
-    response.headers.set('Referrer-Policy', 'unsafe-url')
-    response.headers.set('Feature-Policy', 'none')
-
-    return response
-
-  } catch (e) {
-    // if an error is thrown try to serve the asset at 404.html
-    if (!DEBUG) {
-      try {
-        let notFoundResponse = await getAssetFromKV(event, {
-          mapRequestToAsset: req => new Request(`${new URL(req.url).origin}/404.html`, req),
-        })
-
-        return new Response(notFoundResponse.body, { ...notFoundResponse, status: 404 })
-      } catch (e) {}
+    const parsedURL = new URL(event.request.url);
+    if (parsedURL.pathname === '/') {
+      const lang = getPreferredLanguage(event.request);
+      return Response.redirect(`${parsedURL.origin}/${lang}/`, 303);
     }
 
-    return new Response(e.message || e.toString(), { status: 500 })
+    const page = await getAssetFromKV(event, options);
+
+    // allow headers to be altered
+    const response = new Response(page.body, page);
+
+    if (event.request.url.endsWith('/feed.xml')) {
+      response.headers.set('Content-Type', 'application/rss+xml');
+    }
+    response.headers.set('Content-Security-Policy', "script-src 'self'");
+    response.headers.set('X-XSS-Protection', '1; mode=block');
+    response.headers.set('X-Content-Type-Options', 'nosniff');
+    response.headers.set('X-Frame-Options', 'DENY');
+    response.headers.set('Referrer-Policy', 'unsafe-url');
+    response.headers.set('Feature-Policy', 'none');
+
+    return response
+  } catch (e) {
+    // if an error is thrown try to serve the asset at 404.html
+    if (DEBUG) {
+      console.error("Error fetching resource", e);
+      return new Response(e.message || e.toString(), { status: 500 })
+    }
+
+    let errStatus;
+    if (e instanceof NotFoundError) {
+      errStatus = 404;
+    } else {
+      console.error("Error building response", e);
+      errStatus = 500;
+    }
+
+    function getLanguageFromURLOrPreferred(req, parsedURL) {
+      if (parsedURL.pathname.length <= 3 || parsedURL.pathname[3] === '/') {
+        if (parsedURL.pathname.startsWith('/en')) {
+          return 'en';
+        } else if (parsedURL.pathname.startsWith('/pt')) {
+          return 'pt';
+        }
+      }
+      return getPreferredLanguage(req);
+    }
+
+    function mapToError(req) {
+      const parsedURL = new URL(req.url);
+      const lang = getLanguageFromURLOrPreferred(req, parsedURL);
+      return new Request(`${parsedURL.origin}/${lang}/${errStatus}.html`, req);
+    }
+
+    try {
+      const errorPage = await getAssetFromKV(event, { mapRequestToAsset: mapToError });
+      return new Response(errorPage.body, { ...errorPage, status: errStatus })
+    } catch (e) {
+      console.error(`Failed to find error page ${errStatus}`, e);
+    }
   }
 }
 
-/**
- * Here's one example of how to modify a request to
- * remove a specific prefix, in this case `/docs` from
- * the url. This can be useful if you are deploying to a
- * route on a zone, or if you only want your static content
- * to exist at a specific path.
- */
-function handlePrefix(prefix) {
-  return request => {
-    // compute the default (e.g. / -> index.html)
-    let defaultAssetKey = mapRequestToAsset(request)
-    let url = new URL(defaultAssetKey.url)
-
-    // strip the prefix from the path for lookup
-    url.pathname = url.pathname.replace(prefix, '/')
-
-    // inherit all other props from the default request
-    return new Request(url.toString(), defaultAssetKey)
+function getPreferredLanguage(request) {
+  const acceptLanguage = request.headers.get('Accept-Language') || '';
+  const lang = acceptLanguageParser.pick(SUPPORTED_LANGUAGES, acceptLanguage, { loose: true });
+  if (lang !== null) {
+    return lang;
   }
+  if ((request.cf || {}).country == 'BR') {
+    return 'pt';
+  }
+  return 'en';
 }
