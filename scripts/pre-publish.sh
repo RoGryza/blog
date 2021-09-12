067b46494e5036f21261ae7ed7dd91b0e6a6c417
Rodrigo Gryzinski <contact@rogryza.me>
2021-09-12 20:34:51 +0100

    Update git_ref

--- scripts/pre-publish.sh
+++ scripts/pre-publish.sh
@@ -2,10 +2,10 @@
 
 set -euo pipefail
 
-GITHUB_SHA=${GITHUB_SHA:=$(git rev-parse HEAD)}
+GIT_REF="$(git rev-parse HEAD)"
 
 sed -i 's;^\(\s*\)base-path:.*;\1base-path: "https://blog.rogryza.me";g' config.yml
-sed -i 's;^\(\s*\)git-ref:.*;\1git-ref: "'"$GITHUB_SHA"'";g' config.yml
+sed -i 's;^\(\s*\)git-ref:.*;\1git-ref: "'"$GIT_REF"'";g' config.yml
 
 if [ ! -f wrangler.toml ]; then
   sed 's;^account_id\s*=.*;account_id = "'"$WRANGLER_ACCOUNT_ID"'";g' \
